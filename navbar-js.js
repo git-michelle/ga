a66b5b15d161dc13850bbbbb4f6b87e660a0f236
michellecan <16948138+michellecan@users.noreply.github.com>
2020-08-30 21:16:15 -0700

    semi-complete home, about, donate, contact

---
+++ navbar-js.js
@@ -0,0 +1,34 @@
+(function ($) {
+  // Begin jQuery
+  $(function () {
+    // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $("nav ul li a:not(:only-child)").click(function (e) {
+      $(this).siblings(".nav-dropdown").toggle();
+      // Close one dropdown when selecting another
+      $(".nav-dropdown").not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    // Clicking away from dropdown will remove the dropdown class
+    $("html").click(function () {
+      $(".nav-dropdown").hide();
+    });
+    // Toggle open and close nav styles on click
+    $("#nav-toggle").click(function () {
+      $("nav ul").slideToggle();
+    });
+    // Hamburger to X toggle
+    $("#nav-toggle").on("click", function () {
+      this.classList.toggle("active");
+    });
+  }); // end DOM ready
+})(jQuery); // end jQuery
+
+//to rotate the dropdown menu arrow
+// $(".menu-with-arrow1").click(function () {
+//     $(".arr1").toggleClass("arrow-flip");
+// })
+
+// $(".menu-with-arrow2").click(function () {
+//     $(".arr2").toggleClass("arrow-flip");
+// })
